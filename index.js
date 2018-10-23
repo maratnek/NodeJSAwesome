5ef0152e18a832ab18d5a0786636f8bfce30c992
Marat <marat@MBP-Marat.Dlink>
2018-10-23 23:43:52 +0300

    add github express

--- index.js
+++ index.js
@@ -11,27 +11,28 @@ const GitHub = require('github-api');
 const gh = new GitHub({
    username: pw.username,
    password: pw.password,
-   /* also acceptable:
-      token: 'MY_OAUTH_TOKEN'
-    */
 });
 
 const me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
-me.listNotifications(function(err, notifications) {
-   // do some stuff
-   console.log(notifications);
-});
 
 const clayreimann = gh.getUser('clayreimann');
 clayreimann.listStarredRepos(function(err, repos) {
    // look at all the starred repos!
-   console.log(repos);
+   //console.log(repos);
 });
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  me.listNotifications(function(err, notifications) {
+    // do some stuff
+    //console.log(notifications);
+    res.send(notifications);
+  });
+
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
