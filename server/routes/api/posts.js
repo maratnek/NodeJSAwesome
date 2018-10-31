e1301e228d407500af6fedfb16123f1182333851
Марат Забиров <m.zabirov@innopolis.ru>
2018-10-31 16:13:16 +0300

    refactoring parser

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -6,11 +6,13 @@ const GitHub = require('github-api');
 
 const url = require('url');
 
-const pw = require('./password');
+//const pw = require('./password');
 // basic auth
 const gh = new GitHub({
-  username: pw.username,
-  password: pw.password,
+  username: '',
+  password: '',
+  // username: pw.username,
+  // password: pw.password,
 });
 
 const me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
@@ -21,56 +23,34 @@ const repoInfo = [
   'stargazers_count',
 ];
 
-async function getRepoInfo(link) {
+function getRepoInfo(link) {
   console.log('link: ', link);
   const gitUrl = url.parse(link);
-  if (!gitUrl || gitUrl.host != 'github.com')
-  {
+  if (!gitUrl || gitUrl.host != 'github.com') {
     console.log('error link', gitUrl.host);
     return;
   }
-  let res = gitUrl.path.split(`/`);
-  if(res.length == 3 && res[1] && res[2])
-  {
-    let repos = gh.getRepo(res[1], res[2]);
+  let nodes = gitUrl.path.split(`/`);
+  if(nodes.length == 3 && nodes[1] && nodes[2]) {
+    let repos = gh.getRepo(nodes[1], nodes[2]);
     if (repos)
-    {
-      let info;
-      try {
-        let data = await repos.getDetails();
-        if (data
-          // &&
-          // (repoInfo[0] in data) && (repoInfo[1] in data))
-          // data.hasOwnProperty(repoInfo[0]) &&
-          // data.hasOwnProperty(repoInfo[1]))
-        )
-          {
-        console.log(data);
-            info = {
-              date: data[repoInfo[0]],
-              star: data[repoInfo[1]]
-            };
-            console.log(info);
-            return info;
-          }
-        } catch (err) {
-          //console.log(err);
-        }
-      }
-    }
+      return repos.getDetails();
   }
+}
 
-  const SimpleMD = require('simple-markdown');
+const SimpleMD = require('simple-markdown');
 
-  // Syntax analize
-  // synTree[0] --- heading2
-  // synTree[1] --- list content
-  // synTree[2-n] --- all about list content
-  // ---- heading2
-  // ------- heading3
-  // ------- list
-  // ------------ package or {heading -> package list}
-  repo.getReadme('master', true, (err, data) => {
+// Syntax analize
+// synTree[0] --- heading2
+// synTree[1] --- list content
+// synTree[2-n] --- all about list content
+// ---- heading2
+// ------- heading3
+// ------- list
+// ------------ package or {heading -> package list}
+
+// Get MD NodeJSAwesome and parse
+repo.getReadme('master', true, async (err, data) => {
 
     let mdParse = SimpleMD.defaultBlockParse;
 
@@ -81,18 +61,18 @@ async function getRepoInfo(link) {
     function catContent(sum, it) {
       return sum + (it.content ? it.content : '');
     };
+    // synTree[0] --- heading2
     if (synTree[0].content.reduce(catContent, '') != 'Contents')
       throw 'Exception MD Syntax: Not found ##Contents.';
 
+    // synTree[1] --- list content
     if (synTree[1].type === 'list')
     {
       function getReduceList(list) {
         if (list)
         return list.map(pg => {
-          let content = pg[0].content;
-          let cat = content.reduce(catContent, '');
           return {
-            content: cat,
+            content: pg[0].content.reduce(catContent,''),
             target: pg[0].target.substr(1).toLowerCase(),
             list: [],
           };
@@ -105,8 +85,40 @@ async function getRepoInfo(link) {
       function IsLinkContent(target, link) {
         return (target.content.reduce(catContent,'').toLowerCase() == link);
       }
+      function IsList(node){
+        return (node && node.type && node.type == 'list');
+      }
+      function getAllLinks(node){
+        let packages = [];
+        if (node && node.items)
+          for (let pack of node.items) {
+            console.log(pack);
+            if (pack.length == 3 && pack[0].type == 'link' && pack[2].type == 'list')
+              packages.push({
+                name: pack[2].items.reduce(catContent,''),
+                link: pack[0].target
+              });
+            else if (pack.length == 2 && pack[1].type == 'list')
+              console.log('subitem', pack[1].items);
+      }
+      function findAllHeadLinks(tree, parts) {
+        let targets = [];
+        for (let i = 2; i < tree.length; i++) {
+          for (let target of parts) {
+            if (IsHead(tree[i],3) && IsLinkContent(tree[i], target.target)) {
+              if (i + 1 < tree.length && tree[i+1] && IsList(tree[i+1]))
+                targets.push({
+                  name: target.content,
+                  target: target.target,
+                  links: getAllLinks(tree[i+1]),
+                });
+            }
+          }
+        }
+      }
 
       // check all content link
+      //console.log(synTree);
       let allContents = [];
       for (let section of synTree[1].items) {
         let link = section[0].target.substr(1).toLowerCase();
@@ -116,8 +128,9 @@ async function getRepoInfo(link) {
           {
             console.log(link);
             let parts = getReduceList(section[2].items);
-            let packageFull = findAllPackage(parts);
-            allContents.push({link: link, index: i, packages: packageFull});
+            let links = findAllHeadLinks(synTree, parts);
+            //let packageFull = findAllPackageByTarget(parts, i);
+            //allContents.push({link: link, packages: packageFull});
           }
         }
       }
@@ -130,15 +143,40 @@ async function getRepoInfo(link) {
         return info;
       };
 
+      function getOurProperty(data, property) {
+        if (data.hasOwnProperty(property[0]) &&
+            data.hasOwnProperty(property[1]))
+        {
+            return {
+              date: data[property[0]],
+              star: data[property[1]]
+            };
+        } else {
+          console.error(`Not known prop: ${property}`);
+        }
+      }
       getRepoInfo('https://github.com/mcollina/aedes')
-      .then(data => console.log(data));
+      .then(data => {
+        if (!data)
+          return;
+        let d = getOurProperty(data.data, repoInfo);
+        console.log(d);
+      })
+      .catch(err => console.log('Error get repo info'));
 
-      function findAllPackage(packages) {
-        console.log('Find all packages');
+      let data = await getRepoInfo('https://github.com/mcollina/aedes');
+      if (data){
+        console.log(typeof(data));
+        let d = getOurProperty(data.data, repoInfo);
+        console.log(d);
       }
 
-      return;
+      console.log('Synchronus');
 
+      function findAllPackageByTarget(packages) {
+        console.log('Find all packages');
+      }
+      return;
 
       function getListData(list) {
         let listNew = [];
@@ -150,7 +188,6 @@ async function getRepoInfo(link) {
         } else {
           console.log('Is Not Correct Construction');
         }
-        //return list;
       };
     // check all package and add stars and last update
     console.log('check packages');
