d385486d59e96fabb55518ab79c98836aa05be68
Марат Забиров <m.zabirov@innopolis.ru>
2018-11-02 16:34:05 +0300

    add slice

--- server/routes/api/parser.js
+++ server/routes/api/parser.js
@@ -87,15 +87,110 @@ module.exports = class ParserNodeAwesome {
     this.repoName ='awesome-nodejs';
     this.init(()=>{
       console.log(this.repoName);
-      //this.test();
-      this.getPageFragment(10);
+      this.test();
+      // this.getPageFragment(10);
+      this.pageSlice(6)
     });
   }
 
   test() {
-    let fltrData = this.getRepoDataByFilter(750);
-    console.log(fltrData.length);
-    console.log(JSON.stringify(fltrData,null,2));
+    // let fltrData = this.getRepoDataByFilter(750);
+    // console.log(fltrData.length);
+    // console.log(JSON.stringify(fltrData,null,2));
+
+    this.packages = [
+        {
+          name: 'name1',
+          list: [
+            {name: 'pack1'},
+            {name: 'pack2'},
+            {name: 'pack3'},
+            {name: 'pack4'},
+          ],
+        },
+        {
+          name: 'name2',
+          list: [
+            {name: 'pack5'},
+            {name: 'pack6'},
+            {name: 'pack7'},
+            {name: 'pack8'},
+            {name: 'pack9'},
+            {name: 'pack10'},
+          ],
+        },
+        {
+          name: 'name3',
+          list: [
+            {name: 'pack10'},
+            {name: 'pack12'},
+            {name: 'pack13'},
+          ],
+        },
+        {
+          name: 'name4',
+          list: [
+            {name: 'pack14'},
+            {name: 'pack15'},
+          ],
+        },
+        {
+          name: 'name5',
+          list: [
+            {name: 'pack16'},
+            {name: 'pack17'},
+            {name: 'pack18'},
+            {name: 'pack19'},
+            {name: 'pack20'},
+          ],
+        },
+    ];
+  }
+
+  sanitizeForward(object, key){
+    // console.log(object, key);
+    return Object.keys(object)
+    .filter(k => k != key)
+    .reduce((obj, key) => {
+      obj[key] = object[key];
+      return obj;
+    }, {});
+  }
+
+  packSlice(pack, size = 3) {
+    // console.log(pack, size);
+    let packages = [];
+    let body = this.sanitizeForward(pack, 'list');
+    console.log('body: ', body);
+    let tList = [];
+    let count = 0;
+    for (let it of pack.list) {
+      tList.push(it);
+      if (tList.length == size) {
+        body.list = tList;
+        console.log(body);
+        packages.push(body);
+        tList = [];
+      }
+    }
+    if (tList.length) {
+        body.list = tList;
+        console.log(body);
+        packages.push(body);
+        tList = [];
+    }
+    return packages;
+  }
+
+  pageSlice(count) {
+    // console.log(this.packages, count);
+    // for (let pack of this.packages) {
+      // console.log(pack.name, pack.list.length);
+    // }
+
+    let sPackages = this.packSlice(this.packages[0]);
+    console.log(sPackages);
+
   }
 
   getAllData(){
@@ -104,6 +199,7 @@ module.exports = class ParserNodeAwesome {
     else
       return 'Not parse data';
   }
+
 
   getPageFragment(page_count = 50) {
     console.log('Page Fragment: count -', page_count);
@@ -134,7 +230,7 @@ module.exports = class ParserNodeAwesome {
               //console.log('Type ',  pack.name);
               if (link.type == 'link') {
                 chapList.push(link);
-                if (chapList.length == page_count || )
+                if (chapList.length == page_count )
                   addPage(pack, chapList);
               } else if (link.type == 'list') {
                 // console.log(sanitizeForward(link,'list'));
