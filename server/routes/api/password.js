05d0d046f412551adf724042da8bcfdca8585101
Marat <marat@MacBook-Pro-Marat.local>
2018-11-08 21:16:43 +0300

    password use heroku env

--- server/routes/api/password.js
+++ server/routes/api/password.js
@@ -1,4 +1,12 @@
-module.exports  = {
+let password = {
   username: '',
   password: '',
 };
+
+if (process.ENV) {
+  console.log('Process env:');
+  password.username = process.ENV.GITHUB_USERNAME;
+  password.password = process.ENV.GITHUB_SECRET;
+}
+
+module.exports  = password;
